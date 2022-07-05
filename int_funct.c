bcf582d82e36587ac2ce797d70c0bac8326c8778
chostler24 <connor.hostler@gmail.com>
2022-07-05 14:44:01 -0700

    Added function description

--- int_funct.c
+++ int_funct.c
@@ -1,2 +1,11 @@
 #include "main.h"
 
+/**
+  * int_funct - function that prints an integer.
+  * @args: arg list
+  * Return: the amount of characters to be printed
+  */
+
+int int_funct(va_list args)
+{
+}
