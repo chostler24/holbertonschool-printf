1fc215885306914a83d0bab071be549b32da3627
chostler24 <connor.hostler@gmail.com>
2022-07-05 09:56:51 -0700

    Added printf and putchar functions

---
+++ _printf.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+  * _printf - function that prints strings, chars, and numbers.
+  * @format: what format _printf is using
+  * Return: the amount of characters _printf outputs
+  */
+
+int _printf(const char *format, ...)
+{
+	pf_fn pf_funct[] = {
+		{"c", c_funct},
+		{"s", str_funct},
+		{"%", prcnt_funct},
+		{"d", dig_funct},
+		{"i", int_funct},
+	};
+
+	va_list args;
+
+	int num = 0;
+
+	va_start(args, format);
+
+	num = get_pf(const char *format, va_list args, pf_fn pf_funct[]);
+
+	va_end(args);
+
+	return (num);
+}
